f306921fb190ce8f82b3876160a18df29b2de3c6
AndriiBogomolov <97605054+AndriiBogomolov@users.noreply.github.com>
2022-12-31 13:35:30 +0000

    Deploying to gh-pages from @ AndriiBogomolov/REACT-21-22-Tutorial@08f953c25d20ada47dc86200e8fd2495325dde8d 🚀

---
+++ static/js/main.b229643d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-21-22-tutorial"]=this["webpackJsonpreact-21-22-tutorial"]||[]).push([[0],{13:function(t,e,n){},14:function(t,e,n){"use strict";n.r(e);var c=n(1),r=n.n(c),o=n(3),i=n.n(o),a=n(0),s=function(){return Object(a.jsx)("div",{style:{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",fontSize:40,color:"#010101"},children:"React homework template"})};n(13);i.a.createRoot(document.getElementById("root")).render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(s,{})}))}},[[14,1,2]]]);
+//# sourceMappingURL=main.b229643d.chunk.js.map
